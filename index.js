282650e8cb4d6fca9a63244cd356faecb70c6bc0
Meera Menon <46912602+meeramenon07@users.noreply.github.com>
2023-01-01 22:42:04 +0800

    update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const Editor = props => {
     />
   );
 };
-**/
+
 
 
 const Preview = props => {
@@ -81,3 +81,41 @@ Heres some code, \`<div></div>\`, between 2 backticks.
 
 \`\`\`
 // this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+There's also [links]https://github.com/meeramenon07/), and
+> Block Quotes!
+And if you want to get really crazy, even tables:
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`;
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
+
+
