1ddfcda00d5cddebbccd34514014d40b5cc5e2cf
Meera Menon <46912602+meeramenon07@users.noreply.github.com>
2023-01-02 16:58:46 +0800

    updated index.js

--- index.js
+++ index.js
@@ -1,66 +1,73 @@
-
 marked.setOptions({
-  breaks: true
+  breaks:true 
 });
 
-// Set a function to be used by the marked Renderer, the bit that takes markdown and translates it to html, and create a callback for links to return the link in a diffrent format then the default.
 const renderer = new marked.Renderer();
-
 class App extends React.Component {
-  constructor(props) {
+  constructor(props){
     super(props);
     this.state = {
-      markdown: placeholder
+      markdown: placeholder 
     };
     this.handleChange = this.handleChange.bind(this);
+      
+    
   }
-  handleChange(e) {
+  handleChange(e){
     this.setState({
-      markdown: e.target.value
+      markdown: e.target.value 
     });
   }
-  render() {
-    return (
+  render(){
+    return(
       <div>
-        <h1 className="title">React Markdown Previewer</h1>
-        <div className="AppWrap">
-          <div className="EditorWrap">
-            <Toolbar text="Editor" />
-            <Editor
-              markdown={this.state.markdown}
-              onChange={this.handleChange}
+        <h1 className = "title">REACT MARKDOWN PREVIEWER</h1>
+      
+      <div className = "AppWrap">
+        <div className = "EditorWrap">
+          <Toolbar  text = "Editor"  />
+          <Editor  
+            markdown = {this.state.markdown}
+            onChange = {this.handleChange}
             />
-          </div>
-          <div className="PreviewWrap">
-            <Toolbar text="Preview" />
-            <Preview markdown={this.state.markdown} />
-          </div>
+          
         </div>
+        <div className = "PreviewWrap">
+          <Toolbar   text = "Preview" />
+          <Preview   markdown = {this.state.markdown}  />
+          
+        </div>
+        
       </div>
+     
+      
+    </div>
+    
     );
   }
 }
 
-const Toolbar = props => {
-  return (
-    <div className="toolbar">
+const Toolbar = props =>{
+  return(
+    <div className = "toolbar">
       {props.text}
     </div>
+  
   );
 };
 
-const Editor = props => {
-  return (
-    <textarea
-      id="editor"
-      value={props.markdown}
-      onChange={props.onChange}
-      type="text"
-    />
+const Editor = props =>{
+  return(
+    <textarea 
+      id = "editor"
+      value = {props.markdown}
+      onChange  = {props.onChange}
+      type = "text"
+      
+      />
+  
   );
 };
-
-
 
 const Preview = props => {
   return (
@@ -116,6 +123,4 @@ And here. | Okay. | I think we get it.
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
-
-
 
